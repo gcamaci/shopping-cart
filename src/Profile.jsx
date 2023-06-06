3ea7b59d50f9b14d2a7ad3d6fe5c32070e0450b8
gcamaci <77690094+gcamaci@users.noreply.github.com>
2023-06-06 11:37:41 -0500

    react router installed and set up, nav bar set in components

---
+++ src/Profile.jsx
@@ -0,0 +1,11 @@
+import Navbar from './components/Navbar'
+const Profile = () => {
+    return (
+      <div>
+        <Navbar></Navbar>
+        <h1>Hello from Profile</h1>
+      </div>
+    );
+  };
+  
+  export default Profile;
