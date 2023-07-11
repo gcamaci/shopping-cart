8ed7ca331619c57a6801a305905dec5245068ac9
gcamaci <77690094+gcamaci@users.noreply.github.com>
2023-07-11 14:06:15 -0500

    setting up API call for single event

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,13 +1,21 @@
 import { useEffect } from "react"
-const ShopView = (showCode) => {
+const ShopView = (shopCode) => {
     useEffect(() => {
+        console.log(shopCode)
         const getEventInfo =  async () => {
-            const response = await fetch(`https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=${import.meta.env.VITE_APP_MY_API_KEY}`,{mode:'cors'})
+            try {
+                const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events/${shopCode}.json&apikey=${import.meta.env.VITE_APP_MY_API_KEY}`,{mode:'cors'})
+                const data = await response.json();
+                console.log(data)
+            }catch(error) {
+                console.log(error)
+                throw(error)
+            }
         }
 
         getEventInfo()
 
-    },[showCode])
+    },[shopCode])
     return (
         <div>Hello World</div>
         
