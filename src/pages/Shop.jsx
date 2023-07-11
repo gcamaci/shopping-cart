0ed89fa297096590a97115a52bd1613ad8fde739
gcamaci <77690094+gcamaci@users.noreply.github.com>
2023-07-11 14:54:41 -0500

    fixed api call, shopview rendering twice?

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react"
-const ShopView = (shopCode) => {
+const ShopView = ({shopCode}) => {
     useEffect(() => {
         console.log(shopCode)
         const getEventInfo =  async () => {
             try {
-                const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events/${shopCode}.json&apikey=${import.meta.env.VITE_APP_MY_API_KEY}`,{mode:'cors'})
+                const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?id=${shopCode}&apikey=${import.meta.env.VITE_APP_MY_API_KEY}`,{mode:'cors'})
                 const data = await response.json();
                 console.log(data)
             }catch(error) {
