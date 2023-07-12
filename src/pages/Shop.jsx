a0c0146486bce0efcde475133c145edfa2fb040a
gcamaci <77690094+gcamaci@users.noreply.github.com>
2023-07-12 12:45:33 -0500

    why is my component rendering twice

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,17 +1,26 @@
-import { useEffect } from "react"
-const ShopView = ({shopCode}) => {
+import { useEffect, useState } from "react"
+const ShopView = ({ shopCode }) => {
+    const [eventInfo, setEventInfo] = useState({})
     useEffect(() => {
         console.log(shopCode)
         const getEventInfo =  async () => {
+            const eventOBJ = {}
+            const objtwo = {}
             try {
                 const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?id=${shopCode}&apikey=${import.meta.env.VITE_APP_MY_API_KEY}`,{mode:'cors'})
                 const data = await response.json();
                 console.log(data)
+                
             }catch(error) {
                 console.log(error)
                 throw(error)
             }
         }
+
+        name:"Guiseppe"
+        name: "Desmend"
+
+        Object.assign()
 
         getEventInfo()
 
